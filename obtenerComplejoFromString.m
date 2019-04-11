7334ab6801e1e9f461ca58914e4039dbf1717596
afondevilarevelli <antonny.42@gmail.com>
2019-04-11 02:06:37 -0300

    Update obtenerComplejoFromString.m

--- obtenerComplejoFromString.m
+++ obtenerComplejoFromString.m
@@ -1,13 +1,13 @@
-%Desde un texto con el formato '(a,b)' ó '[c,d]', transforma el texto
-%a un número complejo en forma binómica o polar. Esto sirve más adelante
-%para obtener el número de entrada de la interfaz gráfica
+%Desde un texto con el formato '(a,b)' Ã³ '[c;d]', transforma el texto
+%a un nÃºmero complejo en forma binÃ³mica o polar. Esto sirve mÃ¡s adelante
+%para obtener el nÃºmero de entrada de la interfaz grÃ¡fica
 function [numeroComplejo] = obtenerComplejoFromString(text)
     if contains(text,";") && startsWith(text, "[") && endsWith(text, "]")
         %forma polar
         C = strsplit(text,{'[',']', ','});
         aux = createPolar(str2double(C(2)), str2double(C(3)));   
     elseif contains(text,",") && startsWith(text, "(") && endsWith(text, ")")
-        %forma binómica
+        %forma binÃ³mica
         C = strsplit(text,{'(',')', ','});
         aux = str2double(C(2)) + 1i*str2double(C(3));
     end
