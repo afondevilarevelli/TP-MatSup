d859fee2ba62133095cbf0f2ee62d4f97e72886e
afondevilarevelli <antonny.42@gmail.com>
2019-06-10 00:25:45 -0300

    arreglos bldos

--- polarAString.m
+++
@@ -1,5 +0,0 @@
-function [polarString] = polarAString(numPolar)
-    modString = num2str(numPolar.modulo);
-    angString = num2str(numPolar.angulo);
-    polarString = strcat('[', modString, '; ', angString,']');
-end
--- src/polarAString.m
+++ src/polarAString.m
@@ -1,5 +1,5 @@
 function [numString] = polarAString (numPolar)
     moduloString = num2str(numPolar.modulo);
     argString = num2str(numPolar.angulo);
-    numString = strcat('[',moduloString, ' ;', argString, ']');
+    numString = strcat('[',moduloString, ' ; ', argString, ']');
 end
